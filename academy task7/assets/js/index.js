9b2a754952b3e91837e13640c7420cf9b3ac7413
samuraichik06 <samurairtx2006@gmail.com>
2023-07-12 11:18:40 +0300

    main

--- academy task7/assets/js/index.js
+++ academy task7/assets/js/index.js
@@ -1,4 +1,19 @@
-let Name="John"
-let admin= Name
-alert (admin)
-console.log (admin)
+//First task
+//let num1 = prompt("Ededi daxil edin")
+//let num2 = prompt("Kvadrati daxil edin")
+//let num3 = parseInt(num1)**parseInt(num2)
+//alert(num3)
+//Second task
+//let forSelsi = prompt("Selsi deyerini daxil edin")
+//let faranhayt = +forSelsi * 1.8 + 32
+//alert("Cavab faranheytle " + faranhayt)
+//Third task
+
+
+
+let number = prompt("Pls enter yout number")
+if (isNaN(number)) {
+    number = prompt("Pls enter true number")
+} else {
+    alert("it is ok")
+}
